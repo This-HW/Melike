92ced822f88c307ae417d7db0a585ce57b9c1e96
Yong <didix@naver.com>
2018-09-28 12:12:11 +0900

    슬라이딩 화면 기본설정

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 // import {name as DeadLine} from './app.json';
-import DeadLines from './src/Deadlines';
+// import DeadLines from './src/Deadlines';
+// import {name as DeadLine} from './app.json';
+import Intro from './src/Intro';
 import {name as DeadLine} from './app.json';
 
-AppRegistry.registerComponent(DeadLine, () => DeadLines);
+AppRegistry.registerComponent(DeadLine, () => Intro);
