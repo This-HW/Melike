c9346468e5a98a27df344aafb603e58e32b97e30
Yong <didix@naver.com>
2018-10-01 16:31:46 +0900

    swipe option

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 // import {name as DeadLine} from './app.json';
-// import DeadLines from './src/Deadlines';
-// import {name as DeadLine} from './app.json';
-import Intro from './src/Intro';
+import DeadLines from './src/Deadlines';
+// import Intro from './src/Intro';
 import {name as DeadLine} from './app.json';
 
-AppRegistry.registerComponent(DeadLine, () => Intro);
+AppRegistry.registerComponent(DeadLine, () => DeadLines);
