7e1e366188ce7b1670c36ca13060d3995c8005f1
Yong <didix@naver.com>
2018-10-04 12:24:21 +0900

    add PW change view

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 // import {name as DeadLine} from './app.json';
-import DeadLines from './src/Deadlines';
+// import DeadLines from './src/Deadlines';
 // import Intro from './src/Intro';
+import PwChange from './src/PwChange';
 import {name as DeadLine} from './app.json';
 
-AppRegistry.registerComponent(DeadLine, () => DeadLines);
+AppRegistry.registerComponent(DeadLine, () => PwChange);
