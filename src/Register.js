ab684ecc60ff06ac396ff9018c3d489d4f2bbf87
Yong <didix@naver.com>
2018-10-17 14:33:32 +0900

    가입화면  뷰 추가 (+구글가입)

--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, Text, Image, TouchableOpacity, } from 'react-native';
+import { StyleSheet, View, TextInput, Text, Image, TouchableOpacity, Dimensions } from 'react-native';
 import Slider from "react-native-slider";
 
-export default class PwChange extends React.Component {
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export default class Register extends React.Component {
   
   constructor (props) {
     super (props)
@@ -15,14 +19,14 @@ export default class PwChange extends React.Component {
 
   componentDidUpdate(prevProps, prevState){
     console.log("componentDidUpdate: " + JSON.stringify(prevProps) + " " + JSON.stringify(prevState));
+    console.log("this.windowWidth : " + windowWidth );
+    console.log("this.windowHeight : " + windowHeight );
   }
 
 
-  ifPressed = () => {
+  _ifPressed = () => {
     this.setState({ check: !this.state.check});
   }
-
-
 
   render(){
 
@@ -36,7 +40,7 @@ export default class PwChange extends React.Component {
         
           <View style={styles.grayBox}>
             <Text style={styles.inputTitle}>아이디</Text>
-            <TextInput textContentType="username" style={styles.inputBox} placeholder={"이메일 주소 입력"}></TextInput>
+            <TextInput textContentType="username" secureTextEntry={false} style={styles.inputBox} placeholder={"이메일 주소 입력"}></TextInput>
             <TouchableOpacity><Image source={require("./assets/x_icon_circle.png")} style={styles.x_Icon} /></TouchableOpacity>
           </View>        
 
@@ -53,53 +57,54 @@ export default class PwChange extends React.Component {
               <Image source={require("./assets/x_icon_circle.png")} style={styles.x_Icon} />
               </TouchableOpacity>
           </View>
-
+         
         {/* 4번째 박스는 사이즈 변형 필요 */}
-          <View style={{flexDirection:"row", alignItems:"center"}}>
-            <View style={[styles.grayBox, { width:65, padding: 0, alignItems:"center", justifyContent:"center"}]}>
-              <Text style={[styles.inputTitle, {margin: 0, flex:1}]}>나이</Text>
+          <View style={{flexDirection:"row", alignItems:"center",  marginTop:10, marginBottom:0, }}>
+            <View style={[styles.grayBox, { flex:0.17, padding: 0, alignItems:"center", justifyContent:"center"}]}>
+              <Text style={[styles.inputTitle, {alignSelf:"center", marginRight:0, flex: 1,}]}> 나이</Text>
             </View>
+            {/* 나이 설정 - 슬라이더로 */}
             <Slider
-              value={this.state.age}
-              onValueCHange={value => this.setState({age})}
-              style={{flex:1, marginLeft:0, marginRight: 17, }}
+              age={this.state.age}
+              onValueChange={ age => this.setState({ age }) }
+              style={{flex:0.83, height:windowHeight/10, marginLeft:0, marginRight: 17, }}
               minimumTrackTintColor="rgb(255,144,108)"
               maximumValue={100}
-              minimumValue={1}
+              minimumValue={8}
               step={1}
               maximumTrackTintColor="rgb(249,249,249)"
               value={20}
-              thumbTintColor="rgb(255,144,108)"
-            />
-            {/* <Slider
-                style={{flex:1, marginLeft:0, marginRight: 17, }}
-                minimumTrackTintColor="rgb(255,144,108)"
-                maximumValue={100}
-                minimumValue={1}
-                step={1}
-                maximumTrackTintColor="rgb(249,249,249)"
-                value={20}
-                thumbTintColor="rgb(255,144,108)"
-                // trackStyle={styles.trackStyle}                
-                // thumbImage = {require("./assets/age_box.png")}
-                // thumbTouchSize={{width:2, height:2, }}
-                // thumbTintColor='#0c6692'
-                // thumbStyle={styles.thumbStyle}
-                // thumbTintColor="green"
-                >
-                {Platform.OS === "ios" ? 0.11 : 0.1,}
-                <Image source={require("./assets/age_box.png")} style={styles.slider_image} />
-                </Slider> */}
+              thumbStyle={styles.thumbStyle}
+              thumbTouchSize={{width:40, height:windowHeight/10}}
+            >
+            </Slider>
+            
           </View>
-            <Text>{this.state.age}</Text>
-          <View style={styles.grayBox}>
+          {/* 나이 나타내는 말풍선 삽입 (말풍선 이미지 크기 22*18) / age 도입 전 -> top:-83.5, right:-93.5, 
+          {/* <View style={{ positoin:"absolute", top:-83.5, left: (windowWidth/8.775)+(this.state.age*windowWidth/100/1.485), width:50, height: 40}}>     */}
+          {/* <View style={{backgroundColor:"purple", positoin:"absolute", top: -(windowHeight/7.99), left: ((windowWidth/8.775)+(this.state.age*windowWidth/100/1.485)), width:50, height: 40}}>    
+            <Image source={require("./assets/age_box.png")} style={{position:"absolute", width:55, height: 45, resizeMode:"contain"}}  />
+            <Text style={{color:"white", position:"absolute", right: 8.5, top:10, fontSize:13}}>{this.state.age}세</Text>
+          </View> */}
+
+          <View style={[styles.grayBox, {}]}>
             <Text style={styles.inputTitle}>이름</Text>
-            <TextInput textContentType="name" secureTextEntry={true} style={styles.inputBox} placeholder={"실명 입력"}></TextInput>
+            <TextInput textContentType="name" secureTextEntry={false} style={styles.inputBox} placeholder={"실명 입력"}></TextInput>
             <TouchableOpacity><Image source={require("./assets/x_icon_circle.png")} style={styles.x_Icon} /></TouchableOpacity>
           </View>
 
-          <View style={{flexDirection:"row", justifyContent:"flex-start", alignItems:"center", marginTop:85}}>
-            <TouchableOpacity style={{marginLeft: 27.5, width:15, height:15}} onPress={ this.ifPressed }>
+
+          {/* 나이 나타내는 말풍선 삽입 (말풍선 이미지 크기 22*18) / age 도입 전 -> top:-83.5, right:-93.5, */} 
+          <View style={{ positoin:"absolute", top:(-windowHeight/7.99*1.77), left: ((windowWidth/8.775)+(this.state.age*windowWidth/100/1.485)), width:50, height: 40}}>    
+            <Image source={require("./assets/age_box.png")} style={{position:"absolute", width:55, height: 45, resizeMode:"contain"}}  />
+            <Text style={{color:"white", position:"absolute", right: 8.5, top:10, fontSize:13}}>{this.state.age}세</Text>
+          </View>
+
+
+
+
+          <View style={{flexDirection:"row", justifyContent:"flex-start", alignItems:"center", marginTop:90}}>
+            <TouchableOpacity style={{marginLeft: 27.5, width:15, height:15}} onPress={ this._ifPressed }>
               {this.state.check === false ? <Image source={require("./assets/box.png")} style={{width:15, height:15}}  />
               :<Image source={require("./assets/check_box.png")} style={{width:15, height:15}}  />    }
             </TouchableOpacity>
@@ -118,10 +123,7 @@ export default class PwChange extends React.Component {
 
       </View>
     );
-
   }
-
-
 }
 
 const styles = StyleSheet.create({
@@ -148,7 +150,7 @@ const styles = StyleSheet.create({
     flexDirection:"row",
     borderRadius: 23,
     backgroundColor: "rgba(219, 219, 219, 0.2)",
-    paddingLeft:5,
+    paddingLeft:0,
     paddingRight:15,
     margin: 10,
     alignItems: "center",
@@ -160,7 +162,7 @@ const styles = StyleSheet.create({
     flex:0.35,
     color: "#4f4f4f",
     fontWeight:"normal",
-    fontSize: 16,
+    fontSize: 15,
     letterSpacing: 0.35,
     marginLeft:13,
     marginRight: 0,
@@ -169,9 +171,9 @@ const styles = StyleSheet.create({
     flex:0.65,
     color: "#4f4f4f",
     fontWeight:"normal",
-    fontSize: 16,
+    fontSize: 14,
     letterSpacing: 0.35,
-    height:35,
+    height:55,
     marginLeft:7,
     
   },
@@ -201,25 +203,118 @@ const styles = StyleSheet.create({
     color: "rgb(239,155,89)",
     fontSize:13,
   },
-  trackStyle:{
-    height:2,
-    shadowColor:"rgb(255, 114, 108)",
-    backgroundColor: "rgb(255, 114, 108)",
-  },
   thumbStyle:{
     width: 10,
     height: 10,
-    backgroundColor: '#31a4db',
+    backgroundColor: "#ff726c",
     borderRadius: 10 / 2,
-    shadowColor: '#31a4db',
-    shadowOffset: {width: 0, height: 0},
-    shadowRadius: 2,
+    shadowColor: 'white',
+    shadowOffset: {width: -1.5, height: -0},
+    shadowRadius: 0,
     shadowOpacity: 1,
   },
-  slider_image:{
-    width:30,
-    height:30,
-    position:"absolute",
-    left:15
-  }
 })
+
+
+
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//   },
+//   topLine:{
+//     flex:0.1,
+//     justifyContent:"space-between"
+//   },
+//   contents:{
+//     flex:0.8,
+//     alignItems:"center",
+//     justifyContent:"flex-start",
+//     padding: 15,
+//     alignItems: "stretch",
+//     paddingTop: 50,
+//   },
+//   bottomLine:{
+//     flex:0.1,
+//   },
+//   grayBox:{
+//     height: 45,
+//     flexDirection:"row",
+//     borderRadius: 23,
+//     backgroundColor: "rgba(219, 219, 219, 0.2)",
+//     paddingLeft:0,
+//     paddingRight:15,
+//     margin: 10,
+//     alignItems: "center",
+//     textAlign:"center",
+//     justifyContent:"space-between",
+
+//   },
+//   inputTitle : {
+//     flex:0.35,
+//     color: "#4f4f4f",
+//     fontWeight:"normal",
+//     fontSize: 15,
+//     letterSpacing: 0.35,
+//     marginLeft:13,
+//     marginRight: 0,
+//   },
+//   inputBox:{
+//     flex:0.65,
+//     color: "#4f4f4f",
+//     fontWeight:"normal",
+//     fontSize: 14,
+//     letterSpacing: 0.35,
+//     height:55,
+//     marginLeft:7,
+    
+//   },
+//   x_Icon:{
+//     width: 15,
+//     height: 15,
+//     opacity: 0.7,
+//     backgroundColor: "transparent",
+
+//   },
+//   pinkBox:{
+//     height: 42,
+//     borderRadius:23,
+//     backgroundColor:"#ff726c",
+//     margin: 10,
+//     justifyContent:"center",
+//   },
+//   confirmText:{
+//     textAlign: "center",
+//     color: "#ffffff",
+//     letterSpacing: 0.35,
+//     fontWeight:"normal",
+//     fontSize: 18,
+//     alignSelf:"center",
+//   },
+//   orangeText:{
+//     color: "rgb(239,155,89)",
+//     fontSize:13,
+//   },
+//   trackStyle:{
+//     height:2,
+//     shadowColor:"rgb(255, 114, 108)",
+//     backgroundColor: "rgb(255, 114, 108)",
+//   },
+//   thumbStyle:{
+//     width: 10,
+//     height: 10,
+//     backgroundColor: "#ff726c",
+//     borderRadius: 10 / 2,
+//     shadowColor: 'white',
+//     shadowOffset: {width: -1.5, height: -0},
+//     shadowRadius: 0,
+//     shadowOpacity: 1,
+//   },
+//   slider_image:{
+//     width:30,
+//     height:30,
+//     position:"absolute",
+//     left:15
+//   }
+// })
--- src/Register_google.js
+++ src/Register_google.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, Text, Image, TouchableOpacity} from 'react-native';
+import { StyleSheet, View, TextInput, Text, Image, TouchableOpacity, Dimensions } from 'react-native';
+import Slider from "react-native-slider";
 
-export default class PwChange extends React.Component {
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export default class Register_google extends React.Component {
   
   constructor (props) {
     super (props)
     
     this.state = {
-     check : false
+     check : false,
+     age: 20
     }
   }
 
@@ -15,40 +20,67 @@ export default class PwChange extends React.Component {
     console.log("componentDidUpdate: " + JSON.stringify(prevProps) + " " + JSON.stringify(prevState));
   }
 
-
-  ifPressed = () => {
+  _ifPressed = () => {
     this.setState({ check: !this.state.check});
   }
 
+  _ifPressed = () => {
+    this.setState({ check: !this.state.check});
+  }
 
   render(){
 
     return(
+
       <View style={styles.container}>
         <View style={styles.topLine}>
           {/* 패널 삽입부 */}
         </View>  
         <View style={styles.contents}>
-          <View>
-            <Text style={{fontSize:13}}>데드라인이 당신을 기억해드릴게요!</Text>
-            <Text style={{fontSize=9, }}>앱 사용성을 위해 추가정보를 설정해주세요</Text>
+          <View style={{justifyContent:"center", alignItems:"center", marginBottom:40}}>
+            <Text style={{fontSize:15, margin:3}}>데드라인이 당신을 기억해드릴게요!</Text>
+            <Text style={{fontSize:11}}>앱 사용성을 위해 추가정보를 설정해주세요</Text>
           </View>
-          <View style={[styles.grayBox, { /***********크기 조절 필요**********/   }]}>
-            <Text style={styles.inputTitle}>나이</Text>
-            {/* <TextInput textContentType="password" secureTextEntry={true} style={styles.inputBox} placeholder={"입력하세요"}></TextInput>
-            <TouchableOpacity><Image source={require("./assets/x_icon_circle.png")} style={styles.x_Icon} /></TouchableOpacity> */}
+        {/* 4번째 박스는 사이즈 변형 필요 */}
+          <View style={{flexDirection:"row", alignItems:"center",  marginTop:10, marginBottom:0, }}>
+            <View style={[styles.grayBox, { flex:0.17, padding: 0, alignItems:"center", justifyContent:"center"}]}>
+              <Text style={[styles.inputTitle, {alignSelf:"center", marginRight:0, flex: 1,}]}> 나이</Text>
+            </View>
+            {/* 나이 설정 - 슬라이더로 */}
+            <Slider
+              age={this.state.age}
+              onValueChange={ age => this.setState({ age }) }
+              style={{flex:0.83, height:windowHeight/10, marginLeft:0, marginRight: 17, }}
+              minimumTrackTintColor="rgb(255,144,108)"
+              maximumValue={100}
+              minimumValue={8}
+              step={1}
+              maximumTrackTintColor="rgb(249,249,249)"
+              value={20}
+              thumbStyle={styles.thumbStyle}
+              thumbTouchSize={{width:40, height:windowHeight/10}}
+            >
+            </Slider>
+            
+          </View>
+          {/* 나이 나타내는 말풍선 삽입 (말풍선 이미지 크기 22*18) / age 도입 전 -> top:-83.5, right:-93.5, 
+          {/* <View style={{ positoin:"absolute", top:-83.5, left: (windowWidth/8.775)+(this.state.age*windowWidth/100/1.485), width:50, height: 40}}>     */}
+          <View style={{positoin:"absolute", top: -(windowHeight/7.99), left: ((windowWidth/8.775)+(this.state.age*windowWidth/100/1.485)), width:50, height: 40}}>    
+            <Image source={require("./assets/age_box.png")} style={{position:"absolute", width:55, height: 45, resizeMode:"contain"}}  />
+            <Text style={{color:"white", position:"absolute", right: 8.5, top:10, fontSize:13}}>{this.state.age}세</Text>
           </View>
 
-          <View style={{flexDirection:"row"}}>
-            <TouchableOpacity style={{marginTop:85, marginLeft: 25, width:15, height:15}} onPress={ this.ifPressed }>
+          <View style={{flexDirection:"row", justifyContent:"flex-start", alignItems:"center", marginTop:50}}>
+            <TouchableOpacity style={{marginLeft: 27.5, width:15, height:15}} onPress={ this._ifPressed }>
               {this.state.check === false ? <Image source={require("./assets/box.png")} style={{width:15, height:15}}  />
               :<Image source={require("./assets/check_box.png")} style={{width:15, height:15}}  />    }
             </TouchableOpacity>
-            <Text style={styles.orangeText}>서비스 이용악관</Text><Text>과</Text><Text style={styles.orangeText}> 개인정보 취급방침</Text><Text>에 동의합니다.</Text>
+            <Text style={styles.orangeText}> 서비스 이용악관</Text><Text style={{fontSize:13}}>과</Text>
+            <Text style={styles.orangeText}> 개인정보 취급방침</Text><Text style={{fontSize:13}}>에 동의합니다.</Text>
           </View>
 
           <TouchableOpacity style={styles.pinkBox}>
-            <Text style={styles.confirmText}>확인</Text>
+            <Text style={styles.confirmText}>다음</Text>
           </TouchableOpacity>
 
         </View>
@@ -77,6 +109,7 @@ const styles = StyleSheet.create({
     alignItems:"center",
     justifyContent:"flex-start",
     padding: 15,
+    alignItems: "stretch",
     paddingTop: 50,
   },
   bottomLine:{
@@ -87,7 +120,7 @@ const styles = StyleSheet.create({
     flexDirection:"row",
     borderRadius: 23,
     backgroundColor: "rgba(219, 219, 219, 0.2)",
-    paddingLeft:5,
+    paddingLeft:0,
     paddingRight:15,
     margin: 10,
     alignItems: "center",
@@ -99,7 +132,7 @@ const styles = StyleSheet.create({
     flex:0.35,
     color: "#4f4f4f",
     fontWeight:"normal",
-    fontSize: 16,
+    fontSize: 15,
     letterSpacing: 0.35,
     marginLeft:13,
     marginRight: 0,
@@ -108,9 +141,9 @@ const styles = StyleSheet.create({
     flex:0.65,
     color: "#4f4f4f",
     fontWeight:"normal",
-    fontSize: 16,
+    fontSize: 14,
     letterSpacing: 0.35,
-    height:35,
+    height:55,
     marginLeft:7,
     
   },
@@ -119,13 +152,13 @@ const styles = StyleSheet.create({
     height: 15,
     opacity: 0.7,
     backgroundColor: "transparent",
+
   },
   pinkBox:{
     height: 42,
     borderRadius:23,
     backgroundColor:"#ff726c",
     margin: 10,
-    marginTop:30,
     justifyContent:"center",
   },
   confirmText:{
@@ -138,7 +171,166 @@ const styles = StyleSheet.create({
   },
   orangeText:{
     color: "rgb(239,155,89)",
-    fontSize:9,
-  }
-
+    fontSize:13,
+  },
+  thumbStyle:{
+    width: 10,
+    height: 10,
+    backgroundColor: "#ff726c",
+    borderRadius: 10 / 2,
+    shadowColor: 'white',
+    shadowOffset: {width: -1.5, height: -0},
+    shadowRadius: 0,
+    shadowOpacity: 1,
+  },
 })
+
+//   _ifPressed = () => {
+//     this.setState({ check: !this.state.check});
+//   }
+
+//   render(){
+
+//     return(
+//       <View style={styles.container}>
+//         <View style={styles.topLine}>
+//           {/* 패널 삽입부 */}
+//         </View>  
+//         <View style={styles.contents}>
+//           {/* <View>
+//             <Text style={{fontSize:13}}>데드라인이 당신을 기억해드릴게요!</Text>
+//             <Text style={{fontSize=9}}>앱 사용성을 위해 추가정보를 설정해주세요</Text>
+//           </View> */}
+//         {/* 4번째 박스는 사이즈 변형 필요 */}
+//         <View style={{flexDirection:"row", alignItems:"center",  marginTop:10, marginBottom:0, }}>
+//             <View style={[styles.grayBox, { flex:0.17, padding: 0, alignItems:"center", justifyContent:"center"}]}>
+//               <Text style={[styles.inputTitle, {alignSelf:"center", marginRight:0, flex: 1,}]}> 나이</Text>
+//             </View>
+//             {/* 나이 설정 - 슬라이더로 */}
+//             <Slider
+//               age={this.state.age}
+//               onValueChange={ age => this.setState({ age }) }
+//               style={{flex:0.83, height:windowHeight/10, marginLeft:0, marginRight: 17, }}
+//               minimumTrackTintColor="rgb(255,144,108)"
+//               maximumValue={100}
+//               minimumValue={8}
+//               step={1}
+//               maximumTrackTintColor="rgb(249,249,249)"
+//               value={20}
+//               thumbStyle={styles.thumbStyle}
+//               thumbTouchSize={{width:40, height:windowHeight/10}}
+//             >
+//             </Slider>
+            
+//           </View>
+//           {/* 나이 나타내는 말풍선 삽입 (말풍선 이미지 크기 22*18) / age 도입 전 -> top:-83.5, right:-93.5, 
+//           {/* <View style={{ positoin:"absolute", top:-83.5, left: (windowWidth/8.775)+(this.state.age*windowWidth/100/1.485), width:50, height: 40}}>     */}
+//           <View style={{backgroundColor:"purple", positoin:"absolute", top: -(windowHeight/7.99), left: ((windowWidth/8.775)+(this.state.age*windowWidth/100/1.485)), width:50, height: 40}}>    
+//             <Image source={require("./assets/age_box.png")} style={{position:"absolute", width:55, height: 45, resizeMode:"contain"}}  />
+//             <Text style={{color:"white", position:"absolute", right: 8.5, top:10, fontSize:13}}>{this.state.age}세</Text>
+//           </View>
+
+
+//           <View style={{flexDirection:"row"}}>
+//             <TouchableOpacity style={{marginTop:85, marginLeft: 25, width:15, height:15}} onPress={ this.ifPressed }>
+//               {this.state.check === false ? <Image source={require("./assets/box.png")} style={{width:15, height:15}}  />
+//               :<Image source={require("./assets/check_box.png")} style={{width:15, height:15}}  />    }
+//             </TouchableOpacity>
+//             <Text style={styles.orangeText}>서비스 이용악관</Text><Text>과</Text><Text style={styles.orangeText}> 개인정보 취급방침</Text><Text>에 동의합니다.</Text>
+//           </View>
+
+//           <TouchableOpacity style={styles.pinkBox}>
+//             <Text style={styles.confirmText}>확인</Text>
+//           </TouchableOpacity>
+
+//         </View>
+//         <View style={styles.bottomLine}>
+//           {/* 쓰지는 않지만 틀은 유지 */}
+//         </View>
+
+//       </View>
+//     );
+
+//   }
+
+
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//   },
+//   topLine:{
+//     flex:0.1,
+//     justifyContent:"space-between"
+//   },
+//   contents:{
+//     flex:0.8,
+//     alignItems:"center",
+//     justifyContent:"flex-start",
+//     padding: 15,
+//     paddingTop: 50,
+//   },
+//   bottomLine:{
+//     flex:0.1,
+//   },
+//   grayBox:{
+//     height: 45,
+//     flexDirection:"row",
+//     borderRadius: 23,
+//     backgroundColor: "rgba(219, 219, 219, 0.2)",
+//     paddingLeft:5,
+//     paddingRight:15,
+//     margin: 10,
+//     alignItems: "center",
+//     textAlign:"center",
+//     justifyContent:"space-between",
+
+//   },
+//   inputTitle : {
+//     flex:0.35,
+//     color: "#4f4f4f",
+//     fontWeight:"normal",
+//     fontSize: 16,
+//     letterSpacing: 0.35,
+//     marginLeft:13,
+//     marginRight: 0,
+//   },
+//   inputBox:{
+//     flex:0.65,
+//     color: "#4f4f4f",
+//     fontWeight:"normal",
+//     fontSize: 16,
+//     letterSpacing: 0.35,
+//     height:35,
+//     marginLeft:7,
+    
+//   },
+//   x_Icon:{
+//     width: 15,
+//     height: 15,
+//     opacity: 0.7,
+//     backgroundColor: "transparent",
+//   },
+//   pinkBox:{
+//     height: 42,
+//     borderRadius:23,
+//     backgroundColor:"#ff726c",
+//     margin: 10,
+//     marginTop:30,
+//     justifyContent:"center",
+//   },
+//   confirmText:{
+//     textAlign: "center",
+//     color: "#ffffff",
+//     letterSpacing: 0.35,
+//     fontWeight:"normal",
+//     fontSize: 18,
+//     alignSelf:"center",
+//   },
+//   orangeText:{
+//     color: "rgb(239,155,89)",
+//     fontSize:9,
+//   }
+
+// })
